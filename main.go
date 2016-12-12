5abc5435debaee9a3823b836639caa29e74e0d4c
Sidahmed <sidahmed.malaoui@gmail.com>
2016-12-12 11:09:02 +0100

    Add print function.

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	// "container/list"
 	"fmt"
 	"strconv"
 	"strings"
@@ -13,6 +12,25 @@ type Element struct {
 	next_elt  *Element
 }
 
+
+/* Cette fonction affiche les adresses IP de notre black list. */
+func print(elm *Element, stack [4]int64, byte_pos int) {
+	for elm != nil {
+		/* Si on est dans le dernier byte de notre adresse IP. */
+		if elm.next_byte == nil {
+			fmt.Print(stack[0], ".", stack[1], ".", stack[2], ".", elm.value)
+			fmt.Println()
+		/* Sinon on met la valeur du byte actuel de l'@IP, et on rappel à nouveau la fonction print
+		   avec comme élément le prochain byte de l'@IP. */
+		} else {
+			stack[byte_pos] = elm.value
+			print(elm.next_byte, stack, byte_pos+1)
+		}
+		elm = elm.next_elt
+	}
+}
+
+/* Cette fonction insére une nouvelle adresse IP dans notre black list. */
 func insert(head **Element, ip []string) {
 	/* Condition d'arrêt de la fonction récursive. On s'arrête quand on a finit d'insérer tous les octets de
 	   notre adresse IP. */
@@ -27,7 +45,8 @@ func insert(head **Element, ip []string) {
 		(*head).value = ip_octect
 		(*head).next_elt = nil
 		insert(&((*head).next_byte), ip[1:])
-		/* Dans le cas inverse, on insére l'élement au bon endroit. */
+
+	/* Dans le cas inverse, on insére l'élement au bon endroit. L'insertion est ordonnée.*/
 	} else {
 		elt := *head
 		prev := *head
@@ -35,15 +54,20 @@ func insert(head **Element, ip []string) {
 			prev = elt
 			elt = elt.next_elt
 		}
+		/* Si on a atteint la fin de la liste, alors on crée un nouveau élément et on l'insére. */
 		if elt == nil {
 			elt = new(Element)
 			elt.value = ip_octect
 			elt.next_elt = nil
 			prev.next_elt = elt
 			insert(&(elt.next_byte), ip[1:])
+		/* Sinon si l'ectet existe déjà alors on fait un appel récursive pour insérer le prochain octet. */
 		} else if elt.value == ip_octect {
 			insert(&elt.next_byte, ip[1:])
+		/* Sinon si on a trouver le bon endroit pour insérer l'octet actuel, alors on l'insére et on
+		   fait un appel récursive pour insérer l'octet suivant. */
 		} else if elt.value > ip_octect {
+			/* Cette condition gére le cas ou on veut insérer dans la tête de la liste. */
 			if prev == elt {
 				prev = new(Element)
 				prev.value = ip_octect
@@ -62,12 +86,26 @@ func insert(head **Element, ip []string) {
 
 }
 
-func main() {
-	var ip_table_head *Element
 
-	insert(&ip_table_head, strings.Split("192.168.33.44", "."))
-	insert(&ip_table_head, strings.Split("192.168.55.66", "."))
-	// insert(&ip_list, strings.Split("55.168.55.66", "."))
-	insert(&ip_table_head, strings.Split("88.168.55.66", "."))
-	fmt.Println(ip_table_head)
+func main() {
+	var ip_black_list *Element
+	var ip string
+	n := 1
+
+	fmt.Println("Insérez les @IP (une par ligne) et finissez l'insertion par une ligne vide :")
+	for n != 0 {
+		n, _ = fmt.Scanln(&ip)
+		insert(&ip_black_list, strings.Split(ip, "."))
+	}
+
+	insert(&ip_black_list, strings.Split("192.168.33.44", "."))
+	insert(&ip_black_list, strings.Split("192.168.33.44", "."))
+	insert(&ip_black_list, strings.Split("192.168.55.66", "."))
+	insert(&ip_black_list, strings.Split("19.168.55.66", "."))
+	insert(&ip_black_list, strings.Split("55.168.55.66", "."))
+	insert(&ip_black_list, strings.Split("88.168.55.66", "."))
+	insert(&ip_black_list, strings.Split("35.43.78.100", "."))
+
+	fmt.Println("Les adresses IP de notre black list")
+	print(ip_black_list, [4]int64{0}, 0)
 }
